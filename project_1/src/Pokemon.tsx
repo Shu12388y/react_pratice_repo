9163a6e6a9a9b2b64c68417f52de122c6284ec1c
Shu12388y <spl.sp999@gmail.com>
2023-09-16 23:21:18 +0530

    docker file added

--- project_1/src/Pokemon.tsx
+++ project_1/src/Pokemon.tsx
@@ -1,20 +1,9 @@
-import axios from "axios";
-// async function pokemonImage() {
-//   const randomNumber = Math.floor(Math.random() * 1000);
-//   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNumber}.png`;
-//   const data = axios.get(url);
-//   console.log(data);
-//   console.log(randomNumber);
-
-//   return data;
-// }
-
 function Pokemon() {
   const randomNumber = Math.floor(Math.random() * 1000);
   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNumber}.png`;
   return (
     <>
-      <h1>pokemon Number ${randomNumber}</h1>
+      <h1>pokemon Number {randomNumber}</h1>
       <img src={url} alt="image" />
     </>
   );
--- project_1/src/PokemonCard.tsx
+++ project_1/src/PokemonCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Pokemon from './Pokemon'
 function PokemonCard() {
   return (
